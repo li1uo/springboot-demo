4b4b3a0617dad555a8b7ed7ad4a3a36013a159de
liluo <1173530326@qq.com>
2019-11-24 14:37:04 +0800

    注解配置改成yaml配置

--- springboot-21-nacos/src/main/java/demo/springboot/NacosApplication.java
+++ springboot-21-nacos/src/main/java/demo/springboot/NacosApplication.java
@@ -1,10 +1,8 @@
 package demo.springboot;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@NacosPropertySource(dataId = "blade-dev.yaml", autoRefreshed = true)
 @SpringBootApplication
 public class NacosApplication {
 
