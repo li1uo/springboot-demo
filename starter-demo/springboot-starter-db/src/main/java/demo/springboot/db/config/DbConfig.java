124431a0601ededa95ed244f6f02bc4189570a6b
li1uo <li1uo@foxmail.com>
2020-02-29 22:18:50 +0800

    新增YmlPropertyLoaderFactory

--- starter-demo/springboot-starter-db/src/main/java/demo/springboot/db/config/DbConfig.java
+++ starter-demo/springboot-starter-db/src/main/java/demo/springboot/db/config/DbConfig.java
@@ -1,5 +1,6 @@
 package demo.springboot.db.config;
 
+import demo.springboot.tool.yml.YmlPropertyLoaderFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.PropertySource;
  * @author LILUO
  * @date 2020/02/25
  */
-@PropertySource(value = "classpath:/db-config.yml")
+@PropertySource(value = "classpath:/db-config.yml", factory = YmlPropertyLoaderFactory.class)
 @Configuration
 public class DbConfig {
 
