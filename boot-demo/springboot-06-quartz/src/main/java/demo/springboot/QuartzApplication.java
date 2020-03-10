0a7944fb3f896d774563a55e0e08236cee9e5c0f
li1uo <li1uo@foxmail.com>
2020-03-10 23:09:29 +0800

    quartz方法调整

--- boot-demo/springboot-06-quartz/src/main/java/demo/springboot/QuartzApplication.java
+++ boot-demo/springboot-06-quartz/src/main/java/demo/springboot/QuartzApplication.java
@@ -2,11 +2,13 @@ package demo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author LILUO
  * @date 2020/03/10
  */
+@EnableScheduling
 @SpringBootApplication
 public class QuartzApplication {
 
--- boot-demo/springboot-06-quartz/src/main/java/demo/springboot/config/init/ApplicationListener.java
+++ boot-demo/springboot-06-quartz/src/main/java/demo/springboot/config/init/ApplicationListener.java
@@ -5,24 +5,28 @@ import demo.springboot.domain.ScheduleJob;
 import demo.springboot.service.ITaskService;
 import org.quartz.CronTrigger;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.quartz.JobStoreType;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author LILUO
  * @date 2018/11/14
  */
-@EnableScheduling
+@ConditionalOnProperty(value = "spring.quartz.jobStoreType", havingValue = "memory")
 @Component
 public class ApplicationListener implements CommandLineRunner {
 
-    private static Logger logger = LoggerFactory.getLogger(ApplicationListener.class);
+    public static Logger logger = LoggerFactory.getLogger(ApplicationListener.class);
 
     @Autowired
     private Scheduler scheduleBean;
@@ -30,22 +34,47 @@ public class ApplicationListener implements CommandLineRunner {
     @Autowired
     private ITaskService taskService;
 
+    @Autowired
+    private QuartzProperties quartzProperties;
+
     @Override
     public void run(String... args) {
+        if (quartzProperties.getJobStoreType().equals(JobStoreType.MEMORY)){
+            initData();
+        }else{
+            start();
+        }
+    }
+
+    /**
+     * quartz以内存模式启动时初始化数据
+     */
+    public void initData(){
         // 应用启动之后执行所有可执行的的任务
-        List<ScheduleJob> scheduleJobList = taskService.listTask();
+        List<ScheduleJob> scheduleJobList = taskService.list();
         for (ScheduleJob scheduleJob : scheduleJobList) {
             try {
                 CronTrigger cronTrigger = ScheduleUtil.getCronTrigger(scheduleBean, scheduleJob);
-                if (cronTrigger == null) {
+                if (Objects.isNull(cronTrigger)) {
                     ScheduleUtil.createScheduleJob(scheduleBean, scheduleJob);
                 } else {
                     ScheduleUtil.updateScheduleJob(scheduleBean, scheduleJob);
                 }
                 logger.info("Startup {}-{} success", scheduleJob.getJobGroup(), scheduleJob.getJobName());
             } catch (Exception e) {
-                e.printStackTrace();
+                logger.error("quartz initData error", e);
             }
+        }
+    }
+
+    /**
+     * 启动quartz
+     */
+    public void start(){
+        try {
+            scheduleBean.start();
+        } catch (SchedulerException e) {
+            logger.error("quartz start error", e);
         }
     }
 }
