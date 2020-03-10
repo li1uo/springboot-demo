5cbd3b3ee6036b6db355ba0ed891f69fa0ba6d72
li1uo <li1uo@foxmail.com>
2020-03-10 17:32:16 +0800

    quartz脚本更改 version -> 2.3.1

--- boot-demo/springboot-06-quartz/src/main/java/demo/springboot/QuartzApplication.java
+++ boot-demo/springboot-06-quartz/src/main/java/demo/springboot/QuartzApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author LILUO
+ * @date 2020/03/10
  */
 @SpringBootApplication
-public class QuarzApplication {
+public class QuartzApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QuarzApplication.class, args);
+        SpringApplication.run(QuartzApplication.class, args);
     }
 }
--- boot-demo/springboot-06-quartz/src/test/java/demo/springboot/core/QuartzApplicationTests.java
+++ boot-demo/springboot-06-quartz/src/test/java/demo/springboot/core/QuartzApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class QuarzApplicationTests {
+public class QuartzApplicationTests {
 
     @Test
     public void contextLoads() {
