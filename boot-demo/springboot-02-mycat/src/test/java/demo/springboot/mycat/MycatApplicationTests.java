04cac78f11973d17f93f4e761b7962b426ac9905
li1uo <li1uo@foxmail.com>
2020-04-07 22:31:15 +0800

    新增mycat demo

---
+++ boot-demo/springboot-02-mycat/src/test/java/demo/springboot/mycat/MycatApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springboot.mycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
