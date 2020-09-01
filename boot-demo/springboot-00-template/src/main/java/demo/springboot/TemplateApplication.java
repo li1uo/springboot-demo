f7138a33e51ffc5764af0f1e00644dc8e504dc4d
li1uo <li1uo@foxmail.com>
2020-09-01 16:43:41 +0800

    fix -> 新增template模块

---
+++ boot-demo/springboot-00-template/src/main/java/demo/springboot/TemplateApplication.java
@@ -0,0 +1,16 @@
+package demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LILUO
+ * @date 2020/02/02
+ */
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+}
