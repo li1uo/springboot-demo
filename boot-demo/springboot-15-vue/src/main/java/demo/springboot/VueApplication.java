b5fa979efa24c486ed8c32ed430d8215d6e748bb
li1uo <li1uo@foxmail.com>
2021-02-14 21:04:02 +0800

    feature -> 新增vue template

---
+++ boot-demo/springboot-15-vue/src/main/java/demo/springboot/VueApplication.java
@@ -0,0 +1,16 @@
+package demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LILUO
+ * @date 2021/02/14
+ */
+@SpringBootApplication
+public class VueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueApplication.class, args);
+	}
+}
