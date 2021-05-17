d971727cb27649cba530562cd37a9861a81dfd1a
li1uo <li1uo@foxmail.com>
2021-05-17 19:55:04 +0800

    feature -> test模块

---
+++ boot-demo/springboot-test/src/main/java/demo/springboot/TestApplication.java
@@ -0,0 +1,16 @@
+package demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LILUO
+ * @date 2021/05/17
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
